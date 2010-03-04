e4053126b6683a452071d8aad356bef3003b1322
Glenn Roberts <glenn.roberts@siyelo.com>
2010-03-04 15:55:56 +0200

    add app helper - remove non-gem files

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,5 +1,4 @@
-# Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper
+module BlogPostsHelper
 
   def pluralize_if(count, string)
     result = "#{count.to_s} #{string.pluralize}"
