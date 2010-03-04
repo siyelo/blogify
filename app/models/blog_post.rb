87c2f97f2ab5e59b4e2b881568f2e0571eac5153
Glenn Roberts <glenn.roberts@siyelo.com>
2010-03-04 14:43:24 +0200

    init commit

---
+++ app/models/blog_post.rb
@@ -0,0 +1,64 @@
+require 'posterous'
+
+class BlogPost < ActiveRecord::Base
+
+  cattr_accessor :cache_expiry
+  @@cache_expiry = 4.hours
+
+  cattr_accessor :read_timeout
+  @@read_timeout = 5.seconds
+
+  cattr_accessor :post_limit
+  @@post_limit = 3
+
+  cattr_accessor :debug
+  @@debug = false
+
+  attr_accessible :title, :body, :url, :comment_count, :posted
+
+
+  def self.cache_posts( posterous_user, count = post_limit )
+    posts = self.read_posts( posterous_user, count)
+    self.update_posts(posts)
+  end
+
+  protected
+
+    def self.read_posts( posterous_user, count )
+      posts = []
+      begin
+        timeout(read_timeout) do
+          reader = Posterous::Reader.new(posterous_user, nil, count)
+          posts = reader.response
+          if !reader.response[0]      # then we only got one result
+            posts = [reader.response]  # force it into an hash of hashes
+          end
+        end
+      rescue TimeoutError
+        logger.warn("Timeout accessing Posterous Blog: #{$!}, #{$@}")
+      rescue
+        logger.warn("Error accessing Posterous blog: #{$!}")
+      end
+
+      posts
+    end
+
+    def self.update_posts(posts)
+      unless posts.empty?
+        old_posts = self.find(:all, :order => 'created_at')
+
+        posts.each do |p|
+          b = self.new( :title          => p['title'],
+                        :body           => p['body'],
+                        :url            => p['link'],
+                        :comment_count  => p['commentsCount'].to_i,
+                        :posted         => p['date'].to_date)
+
+          b.save
+          # expire oldest
+          old_posts.pop.destroy unless old_posts.empty?
+        end
+      end
+    end
+
+end
---
+++ db/migrate/20100304115504_create_blog_posts.rb
@@ -0,0 +1,18 @@
+class CreateBlogPosts < ActiveRecord::Migration
+  def self.up
+    create_table :blog_posts do |t|
+      t.string    :type
+      t.string    :title
+      t.text      :body
+      t.integer   :comment_count
+      t.string    :url
+      t.date      :posted
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :blog_posts
+  end
+end
